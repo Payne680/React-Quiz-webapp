a0e9d981bd2e88d9d030c8476debae12dfae98b2
PENN JR <pennjunior680@gmail.com>
2023-02-03 14:09:03 +0100

    Complete setup of my web App

---
+++ src/Pages/QuizPage/QuizPage.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { QuestionConsumer } from '../../Context';
+import './QuizPage.css';
+
+function QuizPage() {
+  return (
+    <QuestionConsumer>
+      {({ quiz, pageNumber, setPageNumber, ansTracker, setAnsTracker }) => {
+        const validateAns = (ans, correctAns) => {
+          if (ans === correctAns) {
+            setAnsTracker({
+              passed: ansTracker.passed + 1,
+              failed: ansTracker.failed,
+            });
+          } else {
+            console.log('wrong answer');
+            setAnsTracker({
+              passed: ansTracker.passed,
+              failed: ansTracker.failed + 1,
+            });
+          }
+        };
+
+        return (
+          quiz.length > 0 && (
+            <div className="main">
+              <div className="quiz">
+                <h1 className="title">
+                  <i>Quiz Game</i>
+                </h1>
+                <h4>Category: {quiz[pageNumber].category}</h4>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: quiz[pageNumber].question,
+                  }}
+                />
+
+                <div className="answers">
+                  <Link
+                    to={
+                      quiz.length - 1 === pageNumber
+                        ? '/result'
+                        : `/quiz/${pageNumber}`
+                    }
+                  >
+                    <button
+                      type="button"
+                      className="true"
+                      value="True"
+                      onClick={(e) => {
+                        validateAns(
+                          e.target.value,
+                          quiz[pageNumber].correct_answer
+                        );
+                        setPageNumber((prevNum) => prevNum + 1);
+                      }}
+                    >
+                      True
+                    </button>
+                  </Link>
+
+                  <Link
+                    to={
+                      quiz.length - 1 === pageNumber
+                        ? '/result'
+                        : `/quiz/${pageNumber}`
+                    }
+                  >
+                    <button
+                      type="button"
+                      className="false"
+                      value="False"
+                      onClick={(e) => {
+                        validateAns(
+                          e.target.value,
+                          quiz[pageNumber].correct_answer
+                        );
+                        setPageNumber((prevNum) => prevNum + 1);
+                      }}
+                    >
+                      False
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          )
+        );
+      }}
+    </QuestionConsumer>
+  );
+}
+
+export default QuizPage;
---
+++ src/Pages/ResultPage/GameOver.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable camelcase */
+/* eslint-disable react/no-danger */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { QuestionConsumer } from '../../Context';
+import './GameOver.css';
+
+function GameOver() {
+  return (
+    <QuestionConsumer>
+      {({ quiz, setPageNumber, ansTracker, setAnsTracker }) => {
+        return (
+          <div className="fin">
+            <h1 className="end">Game Over</h1>
+            <div className="results">
+              <p className="pass">
+                <i>PASSED: {ansTracker.passed} </i>
+              </p>
+              <p className="fail">
+                <i>FAILD: {ansTracker.failed} </i>
+              </p>{' '}
+            </div>
+            <h4>
+              <i>
+                <u>Below are the correct answers</u>
+              </i>
+            </h4>
+            <div>
+              {quiz.map(({ question, correct_answer }) => {
+                return (
+                  <div>
+                    <span
+                      key={question}
+                      dangerouslySetInnerHTML={{ __html: question }}
+                    />{' '}
+                    Answers :
+                    <span
+                      dangerouslySetInnerHTML={{ __html: correct_answer }}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+            <Link to="/">
+              <button
+                type="button"
+                onClick={() => {
+                  setPageNumber(0);
+                  setAnsTracker({ passed: 0, failed: 0 });
+                }}
+              >
+                Retry
+              </button>
+            </Link>
+          </div>
+        );
+      }}
+    </QuestionConsumer>
+  );
+}
+
+export default GameOver;
